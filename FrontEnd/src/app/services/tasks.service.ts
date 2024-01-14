74567d62bd6f93acd6eb7a81e9b774cbc00e1de7
Loris-Clement <la216834@student.helha.be>
2024-01-14 15:43:11 +0100

    Adding environment variable

--- FrontEnd/src/app/services/tasks.service.ts
+++ FrontEnd/src/app/services/tasks.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Task} from "../models/Task";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //get method
+  getAllTask(): Observable<Task[]> {
+    return this.http.get<Task[]>()
+  }
 }
