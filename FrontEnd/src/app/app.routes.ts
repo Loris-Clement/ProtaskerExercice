9d5a2b09b06e6813d67846c118b55979893838d4
Loris-Clement <la216834@student.helha.be>
2024-01-14 13:33:53 +0100

    Creating tasks list component

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {TasksListComponent} from "./pages/tasks-list/tasks-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "tasks/list", component:TasksListComponent}
+];
