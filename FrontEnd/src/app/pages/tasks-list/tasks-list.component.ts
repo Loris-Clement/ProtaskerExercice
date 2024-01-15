10fc461a18addc311b41a2529f82d231d24dccaf
Loris-Clement <la216834@student.helha.be>
2024-01-15 16:34:49 +0100

    Modifying Tasks list component

--- FrontEnd/src/app/pages/tasks-list/tasks-list.component.ts
+++ FrontEnd/src/app/pages/tasks-list/tasks-list.component.ts
@@ -3,21 +3,26 @@ import {PanelModule} from "primeng/panel";
 import { Task } from '../../models/Task';
 import {ButtonModule} from "primeng/button";
 import {TasksService} from "../../services/tasks.service";
+import {TableModule} from "primeng/table";
+import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
+import {AddTaskComponent} from "../../components/add-task/add-task.component";
 
 @Component({
   selector: 'app-tasks-list',
   standalone: true,
   imports: [
     PanelModule,
-    ButtonModule
+    ButtonModule,
+    TableModule
   ],
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css'
 })
 export class TasksListComponent implements OnInit{
   tasksList: Task[]=[];
+  ref: DynamicDialogRef | undefined;
 
-  constructor(private tasksService: TasksService) {
+  constructor(private tasksService: TasksService, public dialogService: DialogService) {
   }
   ngOnInit() {
     this.getAllTasks();
@@ -31,10 +36,27 @@ export class TasksListComponent implements OnInit{
     this.tasksService.getAllTask().subscribe({
       next: response => {
         this.tasksList = response;
-        console.log("Succès : ", response);
       },
       error: err => {
         console.error("Erreur : ", err);
+      }
+    })
+  }
+
+  showAdd() {
+    this.ref = this.dialogService.open(AddTaskComponent, {
+      header: 'Nouvelle tâche',
+      data: {ref: this.ref, refreshTasks: this.refreshTasks.bind(this)}
+    })
+  }
+
+  private refreshTasks(){
+    this.tasksService.getAllTask().subscribe({
+      next: response=> {
+        this.tasksList = response;
+      },
+      error: err => {
+        console.error("Erreur : ",err);
       }
     })
   }
