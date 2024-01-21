99ba5e7a357bc9a0b552677a5bb0a573fbb860ca
Loris-Clement <la216834@student.helha.be>
2024-01-21 16:00:11 +0100

    Adding filter by text

--- FrontEnd/src/app/app.config.ts
+++ FrontEnd/src/app/app.config.ts
@@ -6,7 +6,8 @@ import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {DialogModule} from "primeng/dialog";
 import {DialogService} from "primeng/dynamicdialog";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ConfirmationService} from "primeng/api";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), DialogModule, DialogService, importProvidersFrom([BrowserAnimationsModule])]
+  providers: [provideRouter(routes), provideHttpClient(), DialogModule, DialogService, importProvidersFrom([BrowserAnimationsModule]), ConfirmationService]
 };
